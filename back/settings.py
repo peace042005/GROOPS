022613b5b6c40b86541d3f6d054c06e42fdc7428
trendup <trendup91@gmail.com>
2025-08-16 12:03:37 +0200

    new

--- back/settings.py
+++ back/settings.py
@@ -16,6 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from datetime import timedelta
 import dj_database_url
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ENVIRONMENT = os.getenv("DJANGO_ENV", "production")
 DEBUG = False
@@ -119,10 +122,11 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
